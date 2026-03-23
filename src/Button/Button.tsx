cc7dabc25e05be6b694fe6ec21ae753c63b7d965
yhan <yannick.han92600@gmail.com>
2026-03-23 12:52:15 +0100

    Composant Button

---
+++ src/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Button = (props) => {
+    return (
+        <View>
+            <Text>Benjamin</Text>
+        </View>
+    );
+};
+
+export default Button;
