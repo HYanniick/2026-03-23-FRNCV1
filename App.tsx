e59713ed625af8ef767cc148649f2492fce2d322
yhan <yannick.han92600@gmail.com>
2026-03-23 17:23:37 +0100

    counter

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React from 'react'
 import Button from './src/Button/Button';
 
 export default function App() {
+  let counter=0
   return (
     <View style={styles.container}>
-      <Button />
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>valeur de counter : {counter}</Text>
+      <Button bgColor="red" color="white" onButtonPressed={()=>{
+        counter--;
+        console.log(counter)
+      }} >-1</Button>
+      <Button bgColor="blue" color="white" onButtonPressed={()=>{
+        counter++;
+        console.log(counter);
+      }}>+1</Button>
       <StatusBar style="auto" />
     </View>
   );
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from "react-native";
-
-const Button = (props) => {
-    return (
-        <View>
-            <Text>Benjamin</Text>
-        </View>
-    );
+import { View, Text, Pressable,ToastAndroid } from "react-native";
+import React from 'react'
+import styles from "./Button.styles";
+interface IButtonProps{
+  children:string
+  color?:"white"|"grey"
+  bgColor:"blue"|"red"
+  onButtonPressed:()=>void
+}
+const Button: React.FC<IButtonProps>= ({children, color, bgColor, onButtonPressed=()=>{}}) => {
+  //console.log(props)
+  return (
+    <Pressable onPress={(evt)=>{
+        //console.log(evt);
+        ToastAndroid.show("pressed", ToastAndroid.LONG);
+        onButtonPressed()
+    }} style={[styles.Button, {backgroundColor:bgColor}]}>
+      <Text style={[styles.text,{color:color}]}>{children}</Text>
+    </Pressable>
+  );
 };
-
 export default Button;
