b5ef709677d7a686157679eb71742acf3c8c90f6
yhan <yannick.han92600@gmail.com>
2026-03-23 18:22:13 +0100

    change color on Press

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,5 @@
-import { View, Text, Pressable,ToastAndroid } from "react-native";
-import React from 'react'
+import { Text, Pressable } from "react-native";
+import React, { useEffect, useState } from 'react'
 import styles from "./Button.styles";
 interface IButtonProps{
   children:string
@@ -8,11 +8,38 @@ interface IButtonProps{
   onButtonPressed:()=>void
 }
 const Button: React.FC<IButtonProps>= ({children, color, bgColor, onButtonPressed=()=>{}}) => {
-  
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsActive(false);
+    }, 180);
+    return () => clearTimeout(timeoutId);
+  }, [isActive]);
+
+  const handlePressIn = () => {
+    setIsActive(true);
+  };
+
+
+
   return (
     <Pressable onPress={(evt)=>{
         onButtonPressed()
-    }} style={[styles.Button, {backgroundColor:bgColor}]}>
+    }}
+      onPressIn={handlePressIn}
+      style={[
+        styles.Button, {
+            backgroundColor:bgColor === 'blue'
+              ? isActive
+                ? 'lightblue'
+                : 'blue'
+              : isActive
+                ? '#ff7b7b'
+                : 'red',
+        },
+      ]}
+    >
       <Text style={[styles.text,{color:color}]}>{children}</Text>
     </Pressable>
   );
