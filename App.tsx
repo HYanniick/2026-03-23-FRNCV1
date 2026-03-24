e31db4bb41afb0b3095584abc650e7ed6b8421d0
yhan <yannick.han92600@gmail.com>
2026-03-24 14:55:50 +0100

    filter avec recherche

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { IProduct } from "./src/interfaces/IProducts";
 import ProductsListViewer from "./src/components/ui/ProductsListViewer/ProductsListViewer";
 
 export default function App() {
   const [products, setProducts] = useState<Array<IProduct>>([]);
+  const [search, setSearch] = useState("");
+  
   useEffect(() => {
     fetch(
       `${process.env.EXPO_PUBLIC_API_URL}:${process.env.EXPO_PUBLIC_API_PORT}${
@@ -17,11 +19,31 @@ export default function App() {
 
   return (
     <View style={{ flex: 1 }}>
+      <TextInput
+        style={styles.fieldFind}
+        placeholder="Rechercher un produit..."
+        placeholderTextColor={"grey"}
+        value={search}
+        onChangeText={setSearch}
+      />
+
       <ScrollView>
-        <ProductsListViewer products={products}/>
+        <ProductsListViewer products={products.filter((product) =>
+          product.titre.toLowerCase().includes(search.toLowerCase())
+        )}/>
       </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  fieldFind: {
+    backgroundColor: "#fff",
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    fontSize: 16,
+    borderColor: "#ccc",
+    borderWidth: 1,
+  },
+});
