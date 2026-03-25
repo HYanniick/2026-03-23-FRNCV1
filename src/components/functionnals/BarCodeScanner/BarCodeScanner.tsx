a8fe3ae0095aadd2042389d95c37af7896969e2b
yhan <yannick.han92600@gmail.com>
2026-03-25 15:26:13 +0100

    setup Expo Camera + BarCOde component

---
+++ src/components/functionnals/BarCodeScanner/BarCodeScanner.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+type Props = {}
+
+const BarCodeScanner = () => {
+    return (
+        <View>
+            <Text>BarCodeScanner</Text>
+        </View>
+    )
+}
+
+export default BarCodeScanner
